7f4518ba44c37d8242f8bd1670dc4ddc742477a5
Jeremy Kahn <jeremyckahn@gmail.com>
2010-11-07 16:07:25 -0600

    Added some documentation and a demo page.

--- valEase/js/valEase.js
+++ valEase/js/valEase.js
@@ -5,6 +5,11 @@ by Jeremy Kahn - jeremyckahn@gmail.com
 Idea of animating a detached DOM element inspired from Ben Nadel:
 http://www.bennadel.com/blog/2007-Using-jQuery-s-animate-Method-To-Power-Easing-Based-Iteration.htm
 
+This plugin will call the jQuery animate() method on a value.  It lets you perform animation logic on
+something that isn't in the DOM.  An example of where this is useful: animating something in an HTML5
+canvas.  Shapes that are drawn in a canvas aren't in the DOM, and therefore can't have jquery.animate()
+called on them.  However, you can call $.valEase to achieve the same effect.
+
 Sample usage:
 
 $.valEase({
